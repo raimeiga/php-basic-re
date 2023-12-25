807a3238f06963f39100fefaf89f066e5c6ebff0
raimeiga <yusuke612@hotmail.com>
2023-12-25 16:12:31 +0900

    コミット

--- black_board.php
+++ black_board.php
@@ -1,3 +1,4 @@
+
 <!--反復の順番
 index.php（php文の書き方）
 data.php（データ型と四則演算）　
@@ -18,10 +19,22 @@ http://localhost/php-basic-re/black_board.php
      <title>PHP基礎編</title>
  </head>
  <body>
-  <?php
-    $my_name='侍太郎';
-    echo "私の名前は{$my_name}です。"; 
-  ?>
+ <p>
+        <?php
+        // 変数$numに0～4までのランダムな整数を代入する
+        $num = mt_rand(0, 4);
+        // 問:変数$numの値を出力する（確認用）
+        echo $num;
+        echo '<br>';
+
+
+        // 問:変数$numの値が4であれば、「大当たりです」という文字列を出力するコードを書け
+        //  それ以外のときは、「はずれです」という文字列を出力するコードを書け
+        
+       
+       
+       ?>
+    </p>
  </body>
 </html>
    
